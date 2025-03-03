fbf3a4611dc37a42a33a8fa657fb92ab7badc2e0
ai01010 <ashrafuli.us@gmail.com>
2025-03-02 19:32:41 -0600

    Update UI v1.5.0

--- app/page.js
+++ app/page.js
@@ -563,7 +563,7 @@ export default function Home() {
                   </IconButton>
                 </Stack>
                 <Typography fontStyle="italic" sx={{ pt: 1, color: '#808080', textAlign: 'center' }}>
-                  This bot is designed to assist with certification of aricrafts in accordance with FAA regulations.
+                  This bot is designed to assist with certification of aircrafts in accordance with FAA regulations.
                 </Typography>
               </Box>
             </Stack>
